c7cb81b293e0d1cbb88f3838923346b645003c5d
Yeray Alonso <yeray@steema.com>
2023-02-03 11:11:15 +0100

    Restore .NET 5 Blazor demo

---
+++ Blazor/NET 5/TeeChartWithBlazorNET5/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace TeeChartOnBlazor
+{
+    public class Utils
+    {
+
+        public static string getResource(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            //gets all resource list
+            //string[] names = assembly.GetManifestResourceNames();
+
+            var fullResourceName = "TeeChartOnBlazor.Resources."+ resourceName;
+
+            using (Stream stream = assembly.GetManifestResourceStream(fullResourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
