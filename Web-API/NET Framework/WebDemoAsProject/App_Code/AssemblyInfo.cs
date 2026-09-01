d62bef7be356fa28d21f55325669850b792c4537
Marc Meumann <marc@steema.com>
2026-09-01 10:30:10 +0200

    Demo mods for slow jquery refs.

--- Web-API/NET Framework/WebDemoAsProject/App_Code/AssemblyInfo.cs
+++ Web-API/NET Framework/WebDemoAsProject/App_Code/AssemblyInfo.cs
@@ -14,7 +14,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]		
+[assembly: AssemblyCulture("")]
+[assembly: EntryAssembly]
 
 //
 // Version information for an assembly consists of the following four values:
@@ -27,8 +28,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.0.2040.15118")]
-[assembly:EntryAssembly]
+[assembly: AssemblyVersion("4.0.2040.15118")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
